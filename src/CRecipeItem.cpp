a1d7ae2c7599d3fd6fcae43a5e0714805d3e028f
Xaunder <alex.1993.24@gmail.com>
2025-07-07 19:18:19 +0200

    Remove price from RecipeItem

--- src/CRecipeItem.cpp
+++ src/CRecipeItem.cpp
@@ -1,41 +1,32 @@
 #include "CRecipeItem.h"
 
 #include "ExceptionUtils.h"
-#include "NumberUtils.h"
 #include "JsonUtils.h"
 
 namespace ypp_sm
 {
 
-CRecipeItem::CRecipeItem( std::string_view aName, const count& aCount, const float& aPrice ) try :
+CRecipeItem::CRecipeItem( std::string_view aName, const count& aCount ) try :
 	AKeyable( aName ),
-	mCount( aCount ),
-	mPrice( CheckNonNegativeness( aPrice, "price of each item" ) )
+	mCount( aCount )
 {
 }
 YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating recipe item." )
 
 CRecipeItem::CRecipeItem( const json& aJSON, std::string_view aName ) try :
-	CRecipeItem( aName, ValueFromRequiredJSONKey<count>( aJSON, COUNT_KEY ),
-			ValueFromRequiredJSONKey<float>( aJSON, PRICE_KEY ) )
+	CRecipeItem( aName, ValueFromJSON<count>( aJSON ) )
 {
 }
 YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating recipe item from JSON " << aJSON.dump() )
 
 void CRecipeItem::JSON( json& aJSON ) const noexcept
 {
-	AddToJSONKey( aJSON, mCount, COUNT_KEY );
-	AddToJSONKey( aJSON, mPrice, PRICE_KEY );
+	AddToJSON( aJSON, mCount );
 }
 
 const CRecipeItem::count& CRecipeItem::GetCount() const noexcept
 {
 	return mCount;
-}
-
-const float& CRecipeItem::GetPrice() const noexcept
-{
-	return mPrice;
 }
 
 } // ypp_sm namespace
--- tests/unit/TRecipeItem.cpp
+++ tests/unit/TRecipeItem.cpp
@@ -11,40 +11,6 @@ INITIALIZE_TEST( TRecipeItem )
 
 void TRecipeItem::TestExceptions()
 {
-	// Test member constructor
-	CheckException( []()
-	{
-		CRecipeItem{ "", 12, -1 };
-	}, "The price of each item cannot be negative." );
-
-	// Test JSON constructor
-	CheckException( []()
-	{
-		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"Wood": {
-				"Price": -1
-			}
-		} )", "Wood", "Wood" );
-	}, "key 'Quantity' not found" );
-
-	CheckException( []()
-	{
-		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"Wood": {
-				"Quantity": 12
-			}
-		} )", "Wood", "Wood" );
-	}, "key 'Price' not found" );
-
-	CheckException( []()
-	{
-		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"Wood": {
-				"Quantity": 12,
-				"Price": -1
-			}
-		} )", "Wood", "Wood" );
-	}, "The price of each item cannot be negative." );
 }
 
 std::vector<std::string> TRecipeItem::ObtainedResults() noexcept
@@ -52,33 +18,23 @@ std::vector<std::string> TRecipeItem::ObtainedResults() noexcept
 	std::vector<std::string> result;
 
 	for( const auto& recipeItem : std::set{
-		CRecipeItem{ "White paint", 3, 25 },
-		CRecipeItem{ "White paint", 4, 25 },
-		CRecipeItem{ "Red paint", 1, 12.6 },
-		CRecipeItem{ "Wood", 23, 13 },
+		CRecipeItem{ "White paint", 3 },
+		CRecipeItem{ "White paint", 4 },
+		CRecipeItem{ "Red paint", 1 },
+		CRecipeItem{ "Wood", 23 },
 		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"White paint": {
-				"Quantity": 2,
-				"Price": 155.6
-			}
+			"White paint": 2
 		} )", "White paint", "White paint" ),
 		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"Yellow paint": {
-				"Quantity": 2,
-				"Price": 300
-			}
+			"Yellow paint": 2
 		} )", "Yellow paint", "Yellow paint" ),
 		ValueFromJSONKeyString<CRecipeItem>( R"( {
-			"Black paint": {
-				"Quantity": 1,
-				"Price": 2500.67
-			}
+			"Black paint": 1
 		} )", "Black paint", "Black paint" ),
 		} )
 	{
 		result.emplace_back( std::string{ recipeItem.GetKey() } + ":" );
 		result.emplace_back( " Quantity: " + std::to_string( recipeItem.GetCount() ) );
-		result.emplace_back( " Price: " + std::to_string( recipeItem.GetPrice() ) );
 		ypp_sm::types::IJsonable::json outputJSON;
 		AddToJSONKey( outputJSON, recipeItem, recipeItem.GetKey() );
 		result.push_back( outputJSON.dump( 1, '\t' ) );
@@ -92,48 +48,28 @@ std::vector<std::string> TRecipeItem::ExpectedResults() noexcept
 	return {
 		"Black paint:",
 		" Quantity: 1",
-		" Price: 2500.669922",
 		"{\n"
-		"	\"Black paint\": {\n"
-		"		\"Quantity\": 1,\n"
-		"		\"Price\": 2500.669921875\n"
-		"	}\n"
+		"	\"Black paint\": 1\n"
 		"}",
 		"Red paint:",
 		" Quantity: 1",
-		" Price: 12.600000",
 		"{\n"
-		"	\"Red paint\": {\n"
-		"		\"Quantity\": 1,\n"
-		"		\"Price\": 12.600000381469727\n"
-		"	}\n"
+		"	\"Red paint\": 1\n"
 		"}",
 		"White paint:",
 		" Quantity: 3",
-		" Price: 25.000000",
 		"{\n"
-		"	\"White paint\": {\n"
-		"		\"Quantity\": 3,\n"
-		"		\"Price\": 25.0\n"
-		"	}\n"
+		"	\"White paint\": 3\n"
 		"}",
 		"Wood:",
 		" Quantity: 23",
-		" Price: 13.000000",
 		"{\n"
-		"	\"Wood\": {\n"
-		"		\"Quantity\": 23,\n"
-		"		\"Price\": 13.0\n"
-		"	}\n"
+		"	\"Wood\": 23\n"
 		"}",
 		"Yellow paint:",
 		" Quantity: 2",
-		" Price: 300.000000",
 		"{\n"
-		"	\"Yellow paint\": {\n"
-		"		\"Quantity\": 2,\n"
-		"		\"Price\": 300.0\n"
-		"	}\n"
+		"	\"Yellow paint\": 2\n"
 		"}",
 	};
 }
