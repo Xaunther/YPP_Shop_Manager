205ada6ea84a5a3f4e52fd04732ad8ce5f4be1fd
Xaunder <alex.1993.24@gmail.com>
2025-07-07 07:40:08 +0200

    Add AKeyable types

---
+++ include/types/AKeyable.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+/**
+ * @brief Types defined for \ref AKeyable.
+*/
+namespace ypp_sm::types::AKeyable
+{
+
+//! Type for the internal key type.
+using key_type = std::string;
+
+} // ypp_sm::types::AKeyable namespace
