053f394043d452b8b1e0e8f686b9f34dfbc8b23f
Xaunder <alex.1993.24@gmail.com>
2025-07-08 08:27:15 +0200

    Add CheckPositiveness function

--- include/NumberUtils.h
+++ include/NumberUtils.h
@@ -18,4 +18,17 @@ const auto& CheckNonNegativeness( const auto& aNumber, const std::string_view aN
 }
 YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the " << aNumberDescription << "." )
 
+/**
+ * @brief Checks positiveness of a number.
+ * @param aNumber Number to check.
+ * @param aNumberDescription Number description to add to the error message.
+*/
+const auto& CheckPositiveness( const auto& aNumber, const std::string_view aNumberDescription ) try
+{
+	if( aNumber > decltype( aNumber ){} )
+		return aNumber;
+	throw std::invalid_argument{ "The " + std::string( aNumberDescription.data() ) + " must be positive." };
+}
+YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the " << aNumberDescription << "." )
+
 } // ypp_sm namespace
