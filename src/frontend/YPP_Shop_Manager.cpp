7c273d7adf19426789eb4274aeeb84e6eb31204a
Xaunder <alex.1993.24@gmail.com>
2025-07-08 17:57:35 +0200

    Load JSON from file

--- src/frontend/YPP_Shop_Manager.cpp
+++ src/frontend/YPP_Shop_Manager.cpp
@@ -1,10 +1,36 @@
+#include <fstream>
+
+#include "CDataBase.h"
 #include "frontend/CInputArguments.h"
 
 using namespace ypp_sm;
 using namespace frontend;
 
+namespace
+{
+
+template <typename T> T CreateFromJSONFile( std::string_view aFileName );
+
+} // anonymous namespace
+
 int main( const int aArgsCount, const char** aArgs )
 {
-	CInputArguments( aArgsCount, aArgs );
+	CInputArguments inputArguments( aArgsCount, aArgs );
+	if( !inputArguments.WillRun() )
+		return 0;
+
+	auto dataBase = CreateFromJSONFile<CDataBase>( inputArguments.GetJSONFileName() );
+
 	return 0;
 }
+
+namespace
+{
+
+template <typename T> T CreateFromJSONFile( std::string_view aFileName )
+{
+	return T{ types::IJsonable::json::parse( std::ifstream{ aFileName.data() } ) };
+}
+
+
+} // anonymous namespace
