787e1234f429b1aa0656352f15e67e22afcfdbc4
Xaunder <alex.1993.24@gmail.com>
2025-07-09 08:39:04 +0200

    Add ValueFromJSONFile to JsonUtils

--- src/frontend/YPP_Shop_Manager.cpp
+++ src/frontend/YPP_Shop_Manager.cpp
@@ -1,17 +1,10 @@
-#include <fstream>
-
 #include "CDataBase.h"
 #include "frontend/CInputArguments.h"
 
+#include "JsonUtils.h"
+
 using namespace ypp_sm;
 using namespace frontend;
-
-namespace
-{
-
-template <typename T> T CreateFromJSONFile( std::string_view aFileName );
-
-} // anonymous namespace
 
 int main( const int aArgsCount, const char** aArgs )
 {
@@ -19,18 +12,7 @@ int main( const int aArgsCount, const char** aArgs )
 	if( !inputArguments.WillRun() )
 		return 0;
 
-	auto dataBase = CreateFromJSONFile<CDataBase>( inputArguments.GetJSONFileName() );
+	auto dataBase = ValueFromJSONFile<CDataBase>( inputArguments.GetJSONFileName() );
 
 	return 0;
 }
-
-namespace
-{
-
-template <typename T> T CreateFromJSONFile( std::string_view aFileName )
-{
-	return T{ types::IJsonable::json::parse( std::ifstream{ aFileName.data() } ) };
-}
-
-
-} // anonymous namespace
