6eaad0e2633052dcce086c4686380b013c3783f7
Xaunder <alex.1993.24@gmail.com>
2025-07-18 11:34:37 +0200

    Remove unused modifying methods from CKeySets

--- include/CKeySets.h
+++ include/CKeySets.h
@@ -53,51 +53,6 @@ public:
 	//! Retrieves the \copybrief mKeySets
 	key_sets& KeySets() noexcept;
 
-	/**
-	 * @brief Adds a new key.
-	 * @param aKey Key to add.
-	 * @return Whether the element could be added.
-	 */
-	bool AddKey( std::string_view aKey );
-
-	/**
-	 * @brief Removes a key.
-	 * @param aKey Key to remove.
-	 * @return Whether the element could be removed.
-	 */
-	bool RemoveKey( std::string_view aKey );
-
-	/**
-	 * @brief Retrieves the set associated to a key.
-	 * @details If the key is not found, creates a new one.
-	 * @param aKey Key to access.
-	 */
-	std::set<T> Set( std::string_view aKey );
-
-	/**
-	 * @brief Adds a new element.
-	 * @param aSetName Set where the element should be added to.
-	 * @param aElement Element to be added.
-	 * @return Whether the element could be added.
-	 */
-	bool AddElement( std::string_view aSetName, const T& aElement );
-
-	/**
-	 * @brief Removes an element.
-	 * @param aSetName Group where the element should be removed from.
-	 * @param aElement Element to be removed.
-	 * @return Whether the element could be removed.
-	 */
-	bool RemoveElement( std::string_view aSetName, const T& aElement );
-
-	/**
-	 * @brief Modifies an element.
-	 * @param aSetName Group where the element to be modifies is.
-	 * @param aElement Modified element.
-	 * @return Whether the element could be modified.
-	 */
-	bool ModifyElement( std::string_view aSetName, const T& aElement );
-
 private:
 	//! Sets classified by some key.
 	key_sets mKeySets;
@@ -160,54 +115,6 @@ template <typename T>
 CKeySets<T>::key_sets& CKeySets<T>::KeySets() noexcept
 {
 	return mKeySets;
-}
-
-template <typename T>
-bool CKeySets<T>::AddKey( std::string_view aKey )
-{
-	return mKeySets.emplace( aKey ).second;
-}
-
-template <typename T>
-bool CKeySets<T>::RemoveKey( std::string_view aKey )
-{
-	return mKeySets.erase( aKey ) > 0;
-}
-
-template <typename T>
-std::set<T> CKeySets<T>::Set( std::string_view aKey )
-{
-	return mKeySets[ aKey ];
-}
-
-template <typename T>
-bool CKeySets<T>::AddElement( std::string_view aSetName, const T& aElement )
-{
-	auto found = mKeySets.find( aSetName.data() );
-	if( found == mKeySets.end() )
-		return false;
-	return (*found).second.emplace( aElement ).second;
-}
-
-template <typename T>
-bool CKeySets<T>::RemoveElement( std::string_view aSetName, const T& aElement )
-{
-	auto found = mKeySets.find( aSetName.data() );
-	if( found == mKeySets.end() )
-		return false;
-	return (*found).second.erase( aElement ) > 0;
-}
-
-template <typename T>
-bool CKeySets<T>::ModifyElement( std::string_view aSetName, const T& aElement )
-{
-	auto found = mKeySets.find( aSetName.data() );
-	if( found == mKeySets.end() )
-		return false;
-
-	auto& foundSet = (*found).second;
-	foundSet.extract( aElement );
-	return foundSet.emplace( aElement ).second;
 }
 
 } // ypp_sm namespace
