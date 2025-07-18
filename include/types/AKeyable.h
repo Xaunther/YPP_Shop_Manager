cfdf76bd6f5068e4e1c72f1b8a25e0f3a8d5d9f5
Xaunder <alex.1993.24@gmail.com>
2025-07-18 16:01:03 +0200

    Define comparison object for AKeyable

--- include/types/AKeyable.h
+++ include/types/AKeyable.h
@@ -5,10 +5,35 @@
 /**
  * @brief Types defined for \ref AKeyable.
 */
-namespace ypp_sm::types::AKeyable
+namespace ypp_sm
+{
+
+class AKeyable;
+
+namespace types::AKeyable
 {
 
 //! Type for the internal key type.
 using key_type = std::string;
 
-} // ypp_sm::types::AKeyable namespace
+/**
+ * @brief Define key comparison operators.
+ */
+struct SKeyCompare
+{
+	using is_transparent = void;
+	/**
+	 * @brief Define comparisons for the class.
+	 * @{
+	 */
+	bool operator()( const ypp_sm::AKeyable& aLHS, const ypp_sm::AKeyable& aRHS ) const;
+	bool operator()( const ypp_sm::AKeyable& aLHS, const key_type& aRHS ) const;
+	bool operator()( const key_type& aLHS, const ypp_sm::AKeyable& aRHS ) const;
+	/**
+	 * @}
+	 */
+};
+
+} // ypp_sm
+
+} // types::AKeyable namespace
--- src/AKeyable.cpp
+++ src/AKeyable.cpp
@@ -15,4 +15,19 @@ std::string_view AKeyable::GetKey() const noexcept
 	return mKey;
 }
 
+bool types::AKeyable::SKeyCompare::operator()( const ypp_sm::AKeyable& aLHS, const ypp_sm::AKeyable& aRHS ) const
+{
+	return aLHS < aRHS;
+}
+
+bool types::AKeyable::SKeyCompare::operator()( const ypp_sm::AKeyable& aLHS, const key_type& aRHS ) const
+{
+	return aLHS < aRHS;
+}
+
+bool types::AKeyable::SKeyCompare::operator()( const key_type& aLHS, const ypp_sm::AKeyable& aRHS ) const
+{
+	return aLHS < aRHS;
+}
+
 } // ypp_sm namespace
