292ca5d18d04934fc60fd0529771efc65807c340
Xaunder <alex.1993.24@gmail.com>
2025-07-29 18:36:21 +0200

    Add product prices constructor for a single key

--- src/CProductPrices.cpp
+++ src/CProductPrices.cpp
@@ -8,14 +8,19 @@ namespace ypp_sm
 CProductPrices::CProductPrices( const CDataBase& aDataBase ) try
 {
 	for( const auto& [ key, recipes ] : aDataBase.GetRecipes().GetKeySets() )
-	{
-		product_prices::value_type::second_type productPrices;
-		for( const auto& recipe : recipes )
-			productPrices.emplace( recipe, aDataBase );
-		mPrices.emplace( key, std::move( productPrices ) );
-	}
+		mPrices.merge( std::move( CProductPrices{ aDataBase, key }.mPrices ) );
 }
 YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the product prices." )
+
+CProductPrices::CProductPrices( const CDataBase& aDataBase, std::string_view aKey ) try
+{
+	const auto& recipes = aDataBase.GetRecipes().GetKeySets().at( aKey.data() );
+	product_prices::value_type::second_type productPrices;
+	for( const auto& recipe : recipes )
+		productPrices.emplace( recipe, aDataBase );
+	mPrices.emplace( aKey, std::move( productPrices ) );
+}
+YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the product prices from " << aKey << "category." )
 
 std::string CProductPrices::Description( unsigned int aIndentDepth, char aIndentChar ) const noexcept
 {
