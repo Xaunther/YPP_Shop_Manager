5d6af0dc32a2200be2ab75f0d97edf94ce7becae
Xaunder <alex.1993.24@gmail.com>
2025-07-29 17:10:14 +0200

    Add class members

--- include/CProductPrices.h
+++ include/CProductPrices.h
@@ -15,6 +15,14 @@ class CProductPrices : public IDescriptable
 {
 protected:
 	using product_prices = types::CKeySets::key_sets<CProductPrice>;
+
+public:
+	//! Retrieves the \copybrief mPrices
+	product_prices GetPrices() const noexcept;
+
+private:
+	//! Sets of product prices
+	product_prices mPrices;
 };
 
 } // ypp_sm namespace
--- src/CProductPrices.cpp
+++ src/CProductPrices.cpp
@@ -1,1 +1,11 @@
 #include "CProductPrices.h"
+
+namespace ypp_sm
+{
+
+CProductPrices::product_prices CProductPrices::GetPrices() const noexcept
+{
+	return mPrices;
+}
+
+} // namespace ypp_sm
