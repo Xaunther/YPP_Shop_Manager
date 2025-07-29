47bbf27fe3544d35adf344fdeaf4b1c9e36db9e1
Xaunder <alex.1993.24@gmail.com>
2025-07-29 15:36:52 +0200

    Calculate cost in constructor

--- src/CProductPrice.cpp
+++ src/CProductPrice.cpp
@@ -3,6 +3,7 @@
 #include "CDataBase.h"
 #include "CRecipe.h"
 
+#include <optional>
 #include <sstream>
 
 #include "ExceptionUtils.h"
@@ -13,6 +14,8 @@ namespace ypp_sm
 namespace
 {
 
+using optional_recipe = std::optional<std::reference_wrapper<const CRecipe>>;
+
 /*
  * @brief Find the prices table of an item.
  * @details Raises an exception if the item is not found.
@@ -22,17 +25,34 @@ namespace
 const CPricesTable& GetPricesTable( const types::CRecipe::items::value_type& aItem, const types::CDataBase::prices& aPrices );
 
 /*
+ * @brief Find the prices table of an item.
+ * @details Returns an empty optional if the recipe is not found.
+ * @param aItem Item to find.
+ * @param aRecipes Recipes to look for the item recipe.
+ */
+optional_recipe GetRecipe( const types::CRecipe::items::value_type& aItem, const types::CDataBase::recipes& aRecipes );
+
+/*
  * @brief Calculates the order price of a recipe.
  * @param aRecipe Recipe to calculate its price.
  * @param aDataBase Database to look for ingredients prices.
  */
-types::CPricesTable::int_price CalculateOrderPrice( const CRecipe& aRecipe, const CDataBase& aDatabase );
+types::CPricesTable::int_price CalculateOrderPrice( const CRecipe& aRecipe, const CDataBase& aDataBase );
+
+/*
+ * @brief Calculates the cost of a recipe.
+ * @details It tries to find a recipe for each ingredient, if not found, defaults to its cost price.
+ * @param aRecipe Recipe to calculate its cost.
+ * @param aDataBase Database to look for ingredients cost.
+ */
+types::CPricesTable::price CalculateCost( const CRecipe& aRecipe, const CDataBase& aDataBase );
 
 } // anonymous namespace
 
 CProductPrice::CProductPrice( const CRecipe& aRecipe, const CDataBase& aDataBase ) try :
 	AKeyable( aRecipe.GetKey() ),
-	mOrderPrice( CalculateOrderPrice( aRecipe, aDataBase ) )
+	mOrderPrice( CalculateOrderPrice( aRecipe, aDataBase ) ),
+	mCost( CalculateCost( aRecipe, aDataBase ) )
 {
 }
 YPP_SM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating " << aRecipe.GetKey() << " product price." )
@@ -69,19 +89,52 @@ const CPricesTable& GetPricesTable( const types::CRecipe::items::value_type& aIt
 	throw std::invalid_argument{ std::string{ aItem.GetKey() } + " price table not found." };
 }
 
-types::CPricesTable::int_price CalculateOrderPrice( const CRecipe& aRecipe, const CDataBase& aDatabase )
+optional_recipe GetRecipe( const types::CRecipe::items::value_type& aItem, const types::CDataBase::recipes& aRecipes )
+{
+	for( const auto& [ key, recipes ] : aRecipes.GetKeySets() )
+	{
+		const auto found = recipes.find( types::AKeyable::key_type{ aItem.GetKey() } );
+		if( found != recipes.cend() )
+			return *found;
+	}
+	return {};
+}
+
+types::CPricesTable::int_price CalculateOrderPrice( const CRecipe& aRecipe, const CDataBase& aDataBase )
 {
 	using price = types::CPricesTable::price;
 	price result = 0;
 
 	for( const auto& ingredient : aRecipe.GetItems() )
 	{
-		const auto& priceTable = GetPricesTable( ingredient, aDatabase.GetPrices() );
+		const auto& priceTable = GetPricesTable( ingredient, aDataBase.GetPrices() );
 		result += ( priceTable.GetTax() + static_cast<price>( priceTable.GetUsePrice() ) )
 				* static_cast<price>( ingredient.GetValue() );
 	}
 
 	return static_cast<types::CPricesTable::int_price>( result );
+}
+
+// NOLINTNEXTLINE(misc-no-recursion)
+types::CPricesTable::price CalculateCost( const CRecipe& aRecipe, const CDataBase& aDataBase )
+{
+	using price = types::CPricesTable::price;
+	price result = 0;
+
+	for( const auto& ingredient : aRecipe.GetItems() )
+	{
+		if( const auto recipe = GetRecipe( ingredient, aDataBase.GetRecipes() ) )
+			result += CalculateCost( *recipe, aDataBase ) * static_cast<price>( (*recipe).get().GetYield() );
+		else
+		{
+			const auto& priceTable = GetPricesTable( ingredient, aDataBase.GetPrices() );
+			result += ( priceTable.GetTax() + priceTable.GetCost() ) * static_cast<price>( ingredient.GetValue() );
+		}
+	}
+	result += static_cast<price>( aRecipe.GetDoubloonCount() * aDataBase.GetDoubloonPrice() );
+	result /= static_cast<price>( aRecipe.GetYield() );
+
+	return result;
 }
 
 } // anonymous namespace
