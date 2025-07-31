2f7d0c4a508157ad635048611646e67fdf10034a
Xaunder <alex.1993.24@gmail.com>
2025-07-31 15:36:15 +0200

    Especialize AskInput for containers

--- include/frontend/InputUtils.h
+++ include/frontend/InputUtils.h
@@ -2,13 +2,26 @@
 
 #include <functional>
 #include <iostream>
+#include <set>
 #include <string>
+#include "type_traits"
 
 namespace ypp_sm::frontend
 {
 
 template <typename T> using conversion_function = std::function<T( const std::string& )>;
 template <typename T> using condition_function = std::function<bool( const std::string& )>;
+
+//! Concept to check that the type is a std::vector.
+template <typename T> concept is_vector = std::same_as<std::remove_cvref_t<T>,
+		 std::vector<typename T::value_type, typename T::allocator_type>>;
+//! Concept to check that the type is a std::set.
+template <typename T> concept is_set = std::same_as<std::remove_cvref_t<T>,
+		 std::set<typename T::value_type, typename T::key_compare, typename T::allocator_type>>;
+
+//! Concept to check that the type is a container type.
+template <typename T> concept is_container = is_vector<T> || is_set<T>;
+template <typename T> concept is_not_container = !is_container<T>;
 
 template <typename T> T DEFAULT_CONVERSION( const std::string& aInput );
 template <> unsigned int DEFAULT_CONVERSION( const std::string& aInput );
@@ -21,7 +34,8 @@ template <typename T> bool DEFAULT_CONDITION( const std::string& aInput );
  * @details Uses default condition and conversion.
  * @param aMessage Message before user input.
  */
-template <typename T> T AskInput( std::string_view aMessage );
+template <is_not_container T> T AskInput( std::string_view aMessage );
+template <is_container T> T AskInput( std::string_view aMessage );
 
 /**
  * @brief Asks user for an input.
@@ -51,9 +65,24 @@ template <typename T> bool DEFAULT_CONDITION( const std::string& aInput )
 	return true;
 }
 
-template <typename T> T AskInput( std::string_view aMessage )
+template <is_not_container T> T AskInput( std::string_view aMessage )
 {
 	return AskInput<T>( aMessage, DEFAULT_CONVERSION<T>, DEFAULT_CONDITION<T> );
+}
+
+template <is_container T> T AskInput( std::string_view aMessage )
+{
+	T result;
+	while( AskInput<std::string>( "Add " + std::string{ aMessage } + "? y/[n]:" ) == "y" )
+	{
+		auto input = AskInput<typename T::value_type>(
+				std::string{ aMessage } + " " + std::to_string( result.size() + 1 ) + ":" );
+		if constexpr( is_vector<T> )
+			result.emplace_back( std::move( input ) );
+		else
+			result.emplace( std::move( input ) );
+	}
+	return result;
 }
 
 template <typename T>
